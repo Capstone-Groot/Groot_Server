bddffaffe117bc2d94efe6edc7886db340b0d42d
jbj616 <jbj616@gmail.com>
2020-05-27 23:45:29 +0900

    fix : mediaType

---
+++ src/main/java/com/groot/demo/command/flower/ImageResult.java
@@ -0,0 +1,11 @@
+package com.groot.demo.command.flower;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class ImageResult {
+
+    private String name;
+    private String message;
+
+}
--- src/main/java/com/groot/demo/service/flower/FlowerService.java
+++ src/main/java/com/groot/demo/service/flower/FlowerService.java
@@ -1,10 +1,12 @@
 package com.groot.demo.service.flower;
 
+import com.groot.demo.command.flower.ImageResult;
 import com.groot.demo.dto.FlowerAddDto;
 import com.groot.demo.entity.FlowerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +23,7 @@ public class FlowerService {
         System.out.println(flowerAddDto.getPicture().getResource().getFilename());
 
         HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
 
         return new ResponseEntity<String>("image ok", headers, HttpStatus.CREATED);
     }
