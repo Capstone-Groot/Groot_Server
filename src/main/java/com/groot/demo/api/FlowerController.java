84ca92fd5892e4e1fd032d10d44ccfdf89d22894
jbj616 <jbj616@gmail.com>
2020-05-31 00:25:04 +0900

    feat : flower 분류 기능 추가

--- src/main/java/com/groot/demo/api/FlowerController.java
+++ src/main/java/com/groot/demo/api/FlowerController.java
@@ -3,10 +3,10 @@ package com.groot.demo.api;
 import com.groot.demo.dto.FlowerAddDto;
 import com.groot.demo.service.flower.FlowerService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,8 +17,10 @@ public class FlowerController {
 
     private final FlowerService flowerService;
 
+    private String url = "http://localhost:5000/";
+
     @PostMapping(value = "{userId}/flowers")
-    public ResponseEntity addFlower(MultipartFile picture) {
+    public ResponseEntity addFlower(@RequestParam("picture") MultipartFile picture) throws Exception {
 
         return flowerService.add(FlowerAddDto.builder()
                                              .picture(picture)
--- src/main/java/com/groot/demo/service/flower/FlowerService.java
+++ src/main/java/com/groot/demo/service/flower/FlowerService.java
@@ -1,30 +1,44 @@
 package com.groot.demo.service.flower;
 
-import com.groot.demo.command.flower.ImageResult;
 import com.groot.demo.dto.FlowerAddDto;
 import com.groot.demo.entity.FlowerRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
 
 @RequiredArgsConstructor
 @Service
 public class FlowerService {
 
     private final FlowerRepository flowerRepository;
+    private final RestTemplate restTemplate;
 
-    public ResponseEntity add(FlowerAddDto flowerAddDto) {
+    public ResponseEntity add(FlowerAddDto flowerAddDto) throws IOException {
 
-        //사진 저장하기
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.MULTIPART_FORM_DATA);
 
-        System.out.println(flowerAddDto.getPicture().getResource().getFilename());
+        ByteArrayResource fileResource = new ByteArrayResource(flowerAddDto.getPicture().getBytes()) {
+            @Override
+            public String getFilename() {
+                return "imageFile.jpg";
+            }
+        };
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("file", fileResource);
+
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, httpHeaders);
+        ResponseEntity<String> response = restTemplate.postForEntity("http://127.0.0.1:5000/predict", requestEntity, String.class);
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
-        return new ResponseEntity<String>("image ok", headers, HttpStatus.CREATED);
+        return new ResponseEntity<String>(response.getBody(), headers, HttpStatus.CREATED);
     }
 }
